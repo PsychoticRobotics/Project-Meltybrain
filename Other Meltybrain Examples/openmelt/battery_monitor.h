ea8e857ef04ca8554fcd4517c6ff844d247cd470
Peter Elmer <psychoticroboticspeter@gmail.com>
2025-11-20 21:40:01 -0500

    wefswtesfidjw3refs argh

--- Other Meltybrain Examples/openmelt/battery_monitor.h
+++
@@ -1,6 +0,0 @@
-//check for low battery - but only alarm after certain number of low reads in a row (prevents ADC noise from alarming)
-//(settings in melty_config.h)
-bool battery_voltage_low();
-
-//returns battery voltage
-float get_battery_voltage();
